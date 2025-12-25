9f0c3b4c5456a09ef61f152ab927d434b161d311
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2025-12-26 00:09:06 +0100

    Enhance security by disabling null sessions and SMBv1

--- harden.ps1
+++ harden.ps1
@@ -1,3 +1,8 @@
+#Prevent null sessions
+Write-Host "[+] Disabling null sessions"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1 -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymousSam" -Value 1 -Force
+
 #Disable LLMNR
 Write-Host "[+] Disabling LLMNR"
 if (-not (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient")) { New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT" -Name "DNSClient" -Force } ; Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" -Name "EnableMulticast" -Value 0 -Force
@@ -9,3 +14,7 @@ if (-not (Test-Path "HKLM:\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters
 #Disable NBTNS (Netbios over TCP)
 Write-Host "[+] Disabling NBTNS"
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\tcpip_*' -Name NetbiosOptions -Value 2 -Verbose
+
+#Disabling SMBv1
+Write-Host "[+] Disabling SMBv1"
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
